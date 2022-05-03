fcaf8b50b0e64093eba3d5f2718ceb1eccb272b8
karol-kufka <merlin.nr.123@gmail.com>
2022-05-03 18:41:33 +0200

    Utworzenie repozytorium git'a

---
+++ README.md
@@ -0,0 +1,1 @@
+# PSiZ-zadanie4
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+	cout << "Hello world!" << endl;
+	
+	return 0;
+}
